f3c8779d12e44ecde5cae5f56e2a1b523a81dc3e
asantini <augusto.santini@mirgor.com.ar>
2021-03-31 15:19:05 -0300

    aldsfjsdkljf

--- post_update.sh
+++ post_update.sh
@@ -6,9 +6,42 @@
 # corresponding to the last update
 #
 
+ROOTFS_IMG_FILE="/mnt/UDISK/rootfs.img"
+USR_IMG_FILE="/mnt/UDISK/usr.img"
+
 printf "Post-update script started.\n"
 
 # Real work here
-printf "We should try a dd to overwrite some partition and check that nothing breaks.\n"
+printf "Checking that all files are present...\n"
+
+if ! [[ -f "$ROOTFS_IMG_FILE" ]]; then
+	printf "File corresponding to rootfs image not present at: $ROOTFS_IMG_FILE\n"
+fi
+
+if ! [[ -f "$USR_IMG_FILE" ]]; then
+	printf "File corresponding to usr image not present at: $USR_IMG_FILE\n"
+fi
+
+printf "Reflashing rootfs partition\n"
+dd if=/mnt/UDISK/rootfs.img of=/dev/mmcblk0p2 bs=512
+
+if [[ $? -ne 0 ]]; then
+	printf "Reflash of rootfs partition failed with exit code: $?\n"
+	exit 1
+fi
+
+
+printf "Reflashing usr partition\n"
+dd if=/mnt/UDISK/usr.img of=/dev/mmcblk0p3 bs=512
+
+if [[ $? -ne 0 ]]; then
+	printf "Reflash of usr partition failed with exit code: $?\n"
+	exit 1
+fi
 
 printf "Post-update script finished.\n"
+
+printf "Performing a reboot.\n"
+reboot
+
+exit 0
