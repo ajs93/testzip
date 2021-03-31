efe2ac5d424e012dabc0eec1830f898f8d5e5044
asantini <augusto.santini@mirgor.com.ar>
2021-03-31 12:25:12 -0300

    adlsfj

---
+++ post_update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Post-update script
+#
+# This script will be executed after completing all the file renaming
+# corresponding to the last update
+#
+
+printf "Post-update script started.\n"
+
+# Real work here
+printf "We should try a dd to overwrite some partition and check that nothing breaks.\n"
+
+printf "Post-update script finished.\n"
