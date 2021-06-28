26904b3f6b00754dce82d5a1cf3b77876ead6a83
asantini <augusto.santini@mirgor.com.ar>
2021-06-28 12:40:50 -0300

    adsfadsfasd 0.1.18

--- post_update.sh
+++ post_update.sh
@@ -6,8 +6,9 @@
 # corresponding to the last update
 #
 
-ROOTFS_IMG_FILE="/tmp/rootfs.img"
-USR_IMG_FILE="/tmp/usr.img"
+ROOTFS_IMG_FILE="/tmp/rootfs.fex"
+USR_IMG_FILE="/tmp/usr.fex"
+BOOT_IMG_FILE="/tmp/boot.fex"
 
 printf "Post-update script started.\n"
 
@@ -22,6 +23,11 @@ fi
 # Real work here
 printf "Checking that all files are present...\n"
 
+if ! [[ -f "$BOOT_IMG_FILE" ]]; then
+	printf "File corresponding to usr image not present at: $BOOT_IMG_FILE\n"
+	exit 1
+fi
+
 if ! [[ -f "$ROOTFS_IMG_FILE" ]]; then
 	printf "File corresponding to rootfs image not present at: $ROOTFS_IMG_FILE\n"
 	exit 1
@@ -29,6 +35,14 @@ fi
 
 if ! [[ -f "$USR_IMG_FILE" ]]; then
 	printf "File corresponding to usr image not present at: $USR_IMG_FILE\n"
+	exit 1
+fi
+
+printf "Reflashing boot partition\n"
+dd if=$BOOT_IMG_FILE of=/dev/mmcblk0p1 bs=512
+
+if [[ $? -ne 0 ]]; then
+	printf "Reflash of usr partition failed with exit code: $?\n"
 	exit 1
 fi
 
